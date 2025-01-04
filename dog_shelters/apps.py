4cf33781180658f59cc5f59862c98be7a6d58f96
Daniel Isangedighi <danielisangedighi@outlook.com>
2025-01-04 16:28:47 +0100

    database is to creation and deployment Shell Test of Data creation

--- dog_shelters/apps.py
+++ dog_shelters/apps.py
@@ -1,5 +1,6 @@
-from django.apps import AppConfig
+from django.apps import AppConfig # type: ignore
 
 
 class DogSheltersConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'dog_shelters'
